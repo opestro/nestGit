d78f86108a0f0a633ef20ad07a7bfef3410eed3b
opestro <mahdiharzallah21@gmail.com>
2024-04-22 01:23:40 +0100

    starter pack

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="flex justify-center items-center w-screen h-screen align-middle text-center bg-black text-white">
+        <div>
+            <h1 class=" text-8xl text-yellow-300  italic ">nest<b class="text-cyan-400 not-italic">Git</b></h1> 
+            <div class="type-wrap overflow-hidden">
+                <h2 class="text-2xl italic">{{ typewriter }}</h2>
+            </div>
+            <div class="my-2">
+                <p class="text-base">Contact one of our Founders</p>
+                <nuxt-link to="https://www.linkedin.com/in/mehdi-harzallah-24a535225/" >> LinkedIn <</nuxt-link>
+            </div>
+          
+       
+        </div>
+    </div>
+</template>
+<script setup>
+let typewriter= ref('')
+const words = ["The next lead of webHosting.", "Step into the Lead."];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    typewriter.value = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+        
+      }
+    }
+  } else {
+    typewriter.value = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 100);
+}
+
+type();
+</script>
